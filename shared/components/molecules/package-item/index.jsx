d3d414294842998497d10a22bbfdc29a600d6e01
Michel <michel.hinostroza@gmail.com>
2020-11-13 15:38:39 -0500

    resolve features

--- shared/components/molecules/package-item/index.jsx
+++ shared/components/molecules/package-item/index.jsx
@@ -1,5 +1,19 @@
 import Link from 'next/link';
 
+function Days({ days }) {
+  function getNameDays(day) {
+    switch (day) {
+      case 0:
+        return 'Half-Day';
+      case 1:
+        return 'Full Day';
+      default:
+        return `${day} days`;
+    }
+  }
+  return <h6 className="fs-15 font-weight-bold m-0">{getNameDays(days)}</h6>;
+}
+
 function PackageItem({ id, title, days, slug, thumbnail }) {
   return (
     <Link href={`/package/${slug}`}>
@@ -13,9 +27,7 @@ function PackageItem({ id, title, days, slug, thumbnail }) {
             <div className="row align-items-start justify-content-start">
               <div className="col-12">
                 <h5 className="card-title fs-18 lh-23">{title}</h5>
-                <h6 className="fs-15 font-weight-bold m-0">
-                  {days > 1 ? `${days} days` : 'Half-Day'}
-                </h6>
+                <Days days={days} />
               </div>
             </div>
           </div>
