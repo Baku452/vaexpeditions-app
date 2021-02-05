7478e20b8ad6db2bdba3c57cf19766a3cf2f47c0
Michel <michel.hinostroza@gmail.com>
2021-02-05 17:38:29 -0500

    resolve filter

--- shared/components/organism/itineraries/list.jsx
+++ shared/components/organism/itineraries/list.jsx
@@ -49,7 +49,9 @@ function GalleryWrapper(props) {
 
   return (
     <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Body>{images.length > 0 && <Gallery images={images} navigation pagination={false} />}</Modal.Body>
+      <Modal.Body>
+        {images.length > 0 && <Gallery images={images} navigation pagination={false} />}
+      </Modal.Body>
     </Modal>
   );
 }
@@ -67,43 +69,59 @@ function List({ itineraries }) {
   return (
     <>
       <Collapse open={0}>
-        {itineraries.map((item, index) => (
-          <CollapseContent key={index.toString()} index={index} title={item?.subtitle}>
-            <div className="row pb-4 itinerary">
-              <div className="col-12 col-md-8 order-2 order-md-1">
-                <SmartText text={item?.content} length={item?.limit} />
-                <ul className="itinerary-items pt-2 pb-2 pl-0">
-                  {item.items.map(element => (
-                    <li key={element.id} className="pb-3">
-                      <Icon
-                        component={element.types === 'Meals Included' ? Restaurant : Bed}
-                        viewBox="0 0 512 512"
-                        className="icon-size-m mr-2"
-                      />
-                      <span className="font-weight-bold">{element.types}: </span>
-                      {element.text}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+        {itineraries.map(
+          (item, index) =>
+            item.active && (
+              <CollapseContent
+                key={index.toString()}
+                index={index}
+                title={item?.subtitle}>
+                <div className="row pb-4">
+                  <div className="col-12 col-md-8 order-2 order-md-1">
+                    <SmartText text={item?.content} length={item?.limit} />
+                    <ul className="pt-4 pb-2 pl-0">
+                      {item.items.map(element => (
+                        <li key={element.id} className="pb-3">
+                          <Icon
+                            component={
+                              element.types === 'Meals Included' ? Restaurant : Bed
+                            }
+                            viewBox="0 0 512 512"
+                            className="icon-size-m mr-2"
+                          />
+                          <span className="font-weight-bold">{element.types}: </span>
+                          {element.text}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
 
-              {item.images.length > 0 && (
-                <div className="col-12 offset-md-1 col-md-3 pt-2 order-1 order-md-2 text-right">
-                  <img src={PUBLIC_API + item.images[0].image} className="d-block w-100 fit" alt={item.alt} />
-                  <a
-                    href="/gallery"
-                    onClick={event => openModal(event, item)}
-                    className="btn btn-link fs-16 pt-3 d-block text-right">
-                    View all photos
-                  </a>
+                  {item.images.length > 0 && (
+                    <div className="col-12 offset-md-1 col-md-3 pt-2 order-1 order-md-2">
+                      <img
+                        src={PUBLIC_API + item.images[0].image}
+                        className="d-block w-100 fit"
+                        alt={item.alt}
+                      />
+                      <a
+                        href="/gallery"
+                        onClick={event => openModal(event, item)}
+                        className="btn btn-link fs-16 pt-3 d-block text-right">
+                        View all photos
+                      </a>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          </CollapseContent>
-        ))}
+              </CollapseContent>
+            ),
+        )}
       </Collapse>
 
-      <GalleryWrapper images={images} show={modalShow} onHide={() => setModalShow(false)} />
+      <GalleryWrapper
+        images={images}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </>
   );
 }
