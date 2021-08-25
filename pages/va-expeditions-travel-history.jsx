4ae4e1ade1b1124f490bfdfd7c35dca0f4fcd2c5
Aldair Huamani <aiacos12@outlook.com>
2021-08-25 09:27:11 -0500

    fix pages slug

---
+++ pages/va-expeditions-travel-history.jsx
@@ -0,0 +1,49 @@
+import { Banner, PageContent } from '@/components/index';
+import { Base } from '@/layouts/index';
+
+const PUBLIC_API = process.env.NEXT_PUBLIC_API;
+
+export default function History({
+  destinations,
+  packagetypes,
+  notifications,
+  ourpurpose,
+  pagecontent,
+}) {
+  return (
+    <Base
+      destinations={destinations}
+      packagetypes={packagetypes}
+      notifications={notifications}>
+      <Banner description="" image="/images/history-of-the-valencia.jpg" alt="weq" />
+      <PageContent page={pagecontent[0]} ourpurpose={ourpurpose} />
+    </Base>
+  );
+}
+
+export async function getStaticProps() {
+  const destinationsResponse = await fetch(`${PUBLIC_API}/destinations/`);
+  const destinations = await destinationsResponse.json();
+
+  const packagetypesResponse = await fetch(`${PUBLIC_API}/packagestype/`);
+  const packagetypes = await packagetypesResponse.json();
+
+  const notificationResponse = await fetch(`${PUBLIC_API}/notification/`);
+  const notifications = await notificationResponse.json();
+
+  const ourpurposeResponse = await fetch(`${PUBLIC_API}/ourpurpose/list/`);
+  const ourpurpose = await ourpurposeResponse.json();
+
+  const pagecontentResponse = await fetch(`${PUBLIC_API}/history/`);
+  const pagecontent = await pagecontentResponse.json();
+
+  return {
+    props: {
+      destinations,
+      packagetypes,
+      notifications,
+      ourpurpose,
+      pagecontent,
+    },
+  };
+}
