251561cf7bf09e50d4cd4f262f4b3e55ad536801
Yanela <ayab0703@gmail.com>
2021-12-28 10:05:00 -0500

    Vaex-116 WaysToSave

--- shared/components/molecules/ways_to_save/index.jsx
+++ shared/components/molecules/ways_to_save/index.jsx
@@ -6,77 +6,73 @@ import styles from './index.module.scss';
 function WaysToSave() {
   return (
     <>
-      <section id="ways-to-save" className="container">
+      <section id="ways-to-save" className="container py-4 m-auto">
+        <div className="col-12">
+          <h2 className="text-center mb-5 titleUnderline">Ways To Save </h2>
+        </div>
         <div className="row">
-          <div className="container containerBox pt-4 pb-4">
-            <div className="row mb-2">
-              <div className="col-12">
-                <h2 className="text-center mb-5 titleUnderline">Ways To Save </h2>
-              </div>
-              <div className="col-lg-3 col-sm-12 text-center">
-                <div className={`${styles.item}`}>
-                  <Link href="/our-newsletter">
-                    <div>
-                      <img
-                        alt="subscribe for deals"
-                        src="icons/subscribe_for_deals.svg"
-                        className={`${styles.img} card-img-top`}
-                      />
-                      <div className="card-body">
-                        <a>Subscribe Newsletter</a>
-                      </div>
-                    </div>
-                  </Link>
+          <div className="col-lg-3 col-sm-6 text-center">
+            <div className={`${styles.item}`}>
+              <Link href="/our-newsletter">
+                <div>
+                  <img
+                    alt="subscribe for deals"
+                    src="icons/subscribe_for_deals.svg"
+                    className={`${styles.img} card-img-top`}
+                  />
+                  <div className="card-body">
+                    <a>Subscribe Newsletter</a>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3 col-sm-12 text-center">
-                <div className={`${styles.item}`}>
-                  <Link href="/friends-and-family-discounts">
-                    <div>
-                      <img
-                        alt="friends and family"
-                        src="icons/friends_and_family.svg"
-                        className={`${styles.img} card-img-top`}
-                      />
-                      <div className="card-body">
-                        <a>Friends and Family Discounts</a>
-                      </div>
-                    </div>
-                  </Link>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-3 col-sm-6 text-center">
+            <div className={`${styles.item}`}>
+              <Link href="/friends-and-family-discounts">
+                <div>
+                  <img
+                    alt="friends and family"
+                    src="icons/friends_and_family.svg"
+                    className={`${styles.img} card-img-top`}
+                  />
+                  <div className="card-body">
+                    <a>Friends and Family Discounts</a>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3 col-sm-12 text-center">
-                <div className={`${styles.item}`}>
-                  <Link href="/refer-a-friend">
-                    <div>
-                      <img
-                        alt="referral Credits"
-                        src="icons/refer_a_friends.svg"
-                        className={`${styles.img} card-img-top`}
-                      />
-                      <div className="card-body">
-                        <a>Refer a Friend</a>
-                      </div>
-                    </div>
-                  </Link>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-3 col-sm-6 text-center">
+            <div className={`${styles.item}`}>
+              <Link href="/refer-a-friend">
+                <div>
+                  <img
+                    alt="referral Credits"
+                    src="icons/refer_a_friends.svg"
+                    className={`${styles.img} card-img-top`}
+                  />
+                  <div className="card-body">
+                    <a>Refer a Friend</a>
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3 col-sm-12 text-center">
-                <div className={`${styles.item}`}>
-                  <Link href="/cuopons-discounts">
-                    <div>
-                      <img
-                        alt="coupons and discounts"
-                        src="icons/coupons and discounts.svg"
-                        className={`${styles.img} card-img-top`}
-                      />
-                      <div className="card-body">
-                        <a>Special Promotions</a>
-                      </div>
-                    </div>
-                  </Link>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-3 col-sm-6 text-center">
+            <div className={`${styles.item}`}>
+              <Link href="/cuopons-discounts">
+                <div>
+                  <img
+                    alt="coupons and discounts"
+                    src="icons/coupons and discounts.svg"
+                    className={`${styles.img} card-img-top`}
+                  />
+                  <div className="card-body">
+                    <a>Special Promotions</a>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           </div>
         </div>
